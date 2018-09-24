23f5da18be3d4fa62cb61bae77e6ac78d88a6171
Ezequiel <exbios@live.co.uk>
2018-09-24 01:30:56 -0500

    Comments for explain each function

--- hello.go
+++ hello.go
@@ -48,11 +48,13 @@ type GoStringer interface {
     GoString() string
 }
 
+// Check if ANY has a method GoString first cating to GoStringer
+// if is posible.then check for method GoString (Something like fmt)
 func ToString(any interface{}) (ttS string, err error) {
     if v, ok := any.(GoStringer); ok {
         return v.GoString(), nil
     }
-    switch v := any.(type) {
+    switch v := any.(type) { // Check for the type of "any"
     case int:
       return strconv.Itoa(v), nil
     case float64:
@@ -65,20 +67,23 @@ func ToString(any interface{}) (ttS string, err error) {
     return "", errors.New("TypeError \n")
 }
 
+// Create a new Poppler
+func NewPoppler(name string, role string, age int) *Poppler {
+  return &Poppler{Person: &Person{name, age}, Role: role}
+}
 
 func main() {
-  Boss := &Poppler{
-   Person: &Person{"Jefe", 30},
-   Role: "CEO",
-  }
+  Boss := NewPoppler("Jefe", "CEO", 30)
   Boss.IncreaceAge();
 
-  fmt.Printf("hello, world %s \n", Boss.GetName());
-  fmt.Printf("With Age: %d \n", Boss.Age);
+  fmt.Printf("hello, world %s \n", Boss.GetName())
+  fmt.Printf("With Age: %d \n", Boss.Age)
   fmt.Printf("Clojure StringToInt: %d \n", StringToInt("10")())
-  name, err := ToString(Boss);
+
+  name, err := ToString(Boss)
   if err != nil {
-    fmt.Print(err)
+    log.Panic(err)
   }
-  fmt.Printf("ToString: %s \n", name);
+
+  fmt.Printf("ToString: %s \n", name)
 }
